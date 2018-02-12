7f7b8979bec022afc8cb4575204899d65695f6e8
raoj <156321693@qq.com>
2018-02-12 10:23:18 +0800

    去除标题栏 添加网格显示和瀑布流显示

--- app/src/main/java/com/raoj/recyclerview/MainActivity.java
+++ app/src/main/java/com/raoj/recyclerview/MainActivity.java
@@ -1,16 +1,18 @@
 package com.raoj.recyclerview;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.Window;
+import android.view.WindowManager;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     private RecyclerView recyclerView_one;
     //    private RecyclerView.Adapter mAdapter;
@@ -23,6 +25,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        /**
+         * 具体根据系统定义，frameworks/base/core/java/android/view/View.java
+         * 8-全屏，2-hide navi(home,back…)，1-游戏或电子书模式
+         */
+//        getWindow().getDecorView().setSystemUiVisibility(8);//View.SYSTEM_UI_FLAG_SHOW_FULLSCREEN
         setContentView(R.layout.activity_main);
 
         //开始设置RecyclerView
@@ -31,12 +41,12 @@ public class MainActivity extends AppCompatActivity {
         recyclerView_one.setHasFixedSize(true);
 //        //创建线性布局
 //        mLayoutManager = new LinearLayoutManager(this);
-//        mLayoutManager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
+        RecyclerView.LayoutManager mLayoutManager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);//瀑布流布局
+//        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(this, 2){};//网格布局
 //        //垂直方向
 //        mLayoutManager.setOrientation(OrientationHelper.VERTICAL);
 //        //给RecyclerView设置布局管理器
-//        recyclerView_one.setLayoutManager(mLayoutManager);
-        recyclerView_one.setLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));
+        recyclerView_one.setLayoutManager(mLayoutManager);
         // 设置item动画
         recyclerView_one.setItemAnimator(new DefaultItemAnimator());
         //创建适配器，并且设置
